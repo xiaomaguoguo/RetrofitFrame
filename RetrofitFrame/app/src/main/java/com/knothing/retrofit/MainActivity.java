9caaf942dd3df192c1541846d2cc40f8627d621b
mazhihua <mazhihua@bftv.com>
2017-09-05 16:04:43 +0800

    首次提交

---
+++ RetrofitFrame/app/src/main/java/com/knothing/retrofit/MainActivity.java
@@ -0,0 +1,16 @@
+package com.knothing.retrofit;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by MaZhihua on 2017/9/5.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ RetrofitFrame/app/src/main/java/com/knothing/retrofit/Temp.java
@@ -0,0 +1,13 @@
+package com.knothing.retrofit;
+
+/**
+ * Created by MaZhihua on 2017/9/5.
+ */
+
+public class Temp {
+
+    public static void main(String[] args){
+
+    }
+
+}
