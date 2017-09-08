7b4290f7ecf69f0d3150d1189d7515ee8e47aedc
mazhihua <mazhihua@bftv.com>
2017-09-08 18:22:59 +0800

    基础参数封装

--- RetrofitFrame/retorifitlib/src/main/java/com/bftv/retrofitlib/CommonParams.java
+++ RetrofitFrame/retorifitlib/src/main/java/com/bftv/retrofitlib/CommonParams.java
@@ -6,8 +6,36 @@ package com.bftv.retrofitlib;
  */
 public interface CommonParams {
 
-    //Apptoken
-    String APP_TOKEN = "apptoken";
 
-    
+    String PACKAGE_NAME = "packageName";
+
+    String DO = "do";
+
+    String VR_SUPPORT_PARAMS = "supportvr";
+
+    //来自的平台
+    String FORM = "from";
+
+    String PLATE_FORM_TV = "bftv_android";
+
+    String REQUEST_PLATFORM = "requestplatform";    //传 tv、mobile、web
+
+    String PLATFORM = "platform";
+
+    String SYS_VERSION = "sys_version";
+
+    String SOFTID = "softid";
+
+    String UUID = "uuid";
+
+    String SN = "sn";
+
+    String VERSION_NAME = "version_name";
+
+    String VERSION_CODE = "version_code";
+
+    String MAC_ADDRESS = "mac";
+
+    String SCREEN_PIXEL = "screen_pixel";
+
 }
