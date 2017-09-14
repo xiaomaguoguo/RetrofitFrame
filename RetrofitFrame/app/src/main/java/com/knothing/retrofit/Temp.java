7048275ac15e0c2a1af9e4fa539123720eb29058
mazhihua <mazhihua@bftv.com>
2017-09-14 15:44:58 +0800

    优化

--- RetrofitFrame/app/src/main/java/com/knothing/retrofit/Temp.java
+++ RetrofitFrame/app/src/main/java/com/knothing/retrofit/Temp.java
@@ -1,10 +1,18 @@
 package com.knothing.retrofit;
 
 
+import android.util.AndroidException;
+import android.util.Log;
+
 import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
 
 /**
  * Created by MaZhihua on 2017/9/5.
@@ -18,12 +26,23 @@ public class Temp {
         System.out.println("HASH Values = " + HASH.hashCode());
 
         Observable<String> test = Observable.just("ABC","DEF");
-        test.subscribe(new Consumer<String>() {
+        test.doOnNext(new Consumer<String>() {
+            @Override
+            public void accept(@NonNull String s) throws Exception {
+                System.out.println("AAAA = " + s);
+//                Log.e("MainActivity", "doOnNext:" + Thread.currentThread().getName());
+            }
+        }).subscribe(new Consumer<String>() {
             @Override
             public void accept(@NonNull String s) throws Exception {
                 System.out.println("s = " + s);
             }
-        });
+        }/*, new Consumer<Throwable>() {
+            @Override
+            public void accept(@NonNull Throwable throwable) throws Exception {
+                System.out.println("throwable = " + throwable.getMessage());
+            }
+        }*/);
 
         /*test.subscribe(new Action1<String>() {
             @Override
@@ -49,6 +68,48 @@ public class Temp {
             }
         });
 
+//        doOnNext();
+
+    }
+
+    private static void doOnNext(){
+        Observable.just("A","B","C").map(new Function<String, String>() {
+            @Override
+            public String apply(@NonNull String s) throws Exception {
+                return s;
+            }
+        }).doOnNext(new Consumer<String>() {
+            @Override
+            public void accept(@NonNull String s) throws Exception {
+                Log.e("MainActivity", "doOnNext:" + s);
+                Log.e("MainActivity", "doOnNext:" + Thread.currentThread().getName());
+            }
+        })
+//        .observeOn(Schedulers.io())
+//        .subscribeOn(AndroidSchedulers.mainThread())
+        .subscribe(new Observer<String>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+                Log.e("MainActivity", "onSubscribe");
+            }
+
+            @Override
+            public void onNext(@NonNull String s) {
+                Log.e("MainActivity", "print = " + s);
+                Log.e("MainActivity", "subscribe():" + Thread.currentThread().getName());
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                Log.e("MainActivity", "onError");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.e("MainActivity", "onCompleted");
+            }
+        })
+        ;
     }
 
 }
